8b4a03ece43f216e78c829441b84e74e1abafa88
SerhiiKutovyi <wagenrex@gmail.com>
2025-09-27 17:22:28 +0300

    test hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export async function getImagesByQuery(query, page = 1) {
+  const response = await axios.get('', {
+    params: {
+      key: API_KEY,
+      q: `${query}`,
+      image_type: 'photo',
+      page,
+      per_page: 15,
+    },
+  });
+  return response.data;
+}
--- src/main.js
+++ src/main.js
@@ -1,25 +1,22 @@
-import axios from 'axios';
+import { getImagesByQuery } from './js/pixabay-api';
 
-const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+const refs = {
+  form: document.querySelector('.form'),
+  gallery: document.querySelector('.gallery'),
+};
 
 let page = 1;
 
-const getImagesByQuery = async (query, page = 1) => {
-  try {
-    const response = await axios.get('', {
-      params: {
-        key: API_KEY,
-        q: `${query}`,
-        image_type: 'photo',
-        page,
-        per_page: 15,
-      },
-    });
-    console.log(response);
-  } catch (error) {
-    alert(`${error.message}`);
-  }
-};
+refs.form.addEventListener('submit', handleSubmit);
 
-getImagesByQuery('cat', page);
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const query = e.target.elements.search.value;
+
+  getImagesByQuery(`${query}`, page)
+    .then(res => console.log(res))
+    .catch(error => {
+      console.log(error.message);
+    });
+}
